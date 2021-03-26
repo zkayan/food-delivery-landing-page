8b56ab65beede2d0e10186ef070e4235b137efcd
Kayan Chagas <kayan_edu@hotmail.com>
2021-03-26 00:21:05 -0300

    add surge

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+require('@/assets/scss/_style.scss')
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/store/modules/api.store.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+
+// Initial state
+const initialState = () => ({
+  api: [],
+})
+
+// State object
+const state = initialState()
+
+// Getters
+const getters = {
+  getApi(state) {
+    return state.api
+  },
+}
+
+// Actions
+const actions = {
+	clearState({ commit }) { commit('RESET') },
+  loadApi({ commit }) {
+    axios.get(`https://6059fb9db11aba001745d43f.mockapi.io/api/v1/cards`)
+      .then(response => {
+        commit('setApi', response)
+      })
+  },
+}
+
+// Mutations
+const mutations = {
+  RESET(state) {
+    const newState = initialState()
+    Object.keys(newState).forEach(key => {
+      state[key] = newState[key]
+    })
+  },
+  setApi(state, data) {
+    state.movies = data
+  },
+}
+
+// Module exports
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
